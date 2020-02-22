f1df15eb91c495907d59402c28079c0f44ee5fac
MGmoket <lsziun@126.com>
2020-02-22 15:45:25 +0800

    Better solution for problem 95.

--- LeetCode/p95.cpp
+++ LeetCode/p95.cpp
@@ -27,7 +27,7 @@ using namespace std;
 
 namespace p95 {
 
-    class Solution {
+    class Solution0 {
     public:
         int numTrees(int n) {
             vector<int> rcd(n + 1);
@@ -48,6 +48,22 @@ namespace p95 {
             return rcd[n] = nums;
         }
     };
+
+    class Solution {
+    public:
+        int numTrees(int n) {
+            vector<int> dp(n + 1, 0);
+            dp[0] = 1;
+            dp[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+                for (int j = 1; j <= i; j++) {
+                    dp[i] += dp[j - 1] * dp[i - j];
+                }
+            return dp[n];
+        }
+    };
+
 
 }
 
